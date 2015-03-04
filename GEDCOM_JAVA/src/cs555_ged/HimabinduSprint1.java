1d9cfd6febaa8a3a75a18276b39e5a316873fd22
Himabindu <hchaturv@stevens.edu>
2015-03-03 23:59:02 -0500

    Himabindu Sprint1

--- GEDCOM_JAVA/src/cs555_ged/HimabinduSprint1.java
+++ GEDCOM_JAVA/src/cs555_ged/HimabinduSprint1.java
@@ -1,10 +1,10 @@
 package cs555_ged;
 
-public class MissingBirthDateUserCase{
+public class HimabinduSprint1{
 public static void missingBirthDate(IndvidualStruct.individual indArr[], int Itotal){
 	boolean missing = true;
 	System.out.println("User story : Looking for any Missing birth dates:");
-	for(int i=0;i<Itotal+1;i++)
+	for(int i=0;i<Itotal;i++)
 	{
 		if (indArr[i].BDay == null)
 			System.out.println("Birth date is misssing for Individual" + indArr[i].ID);
